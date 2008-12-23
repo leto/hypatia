3cfaca7c05d424ac4e7615ea738be52b302ac877
Duke Leto <jonathan@leto.net>
2008-12-23 03:27:35 -0800

    Refactor sampleFunction

--- js/hypatia.js
+++ js/hypatia.js
@@ -5,4 +5,17 @@
             debug( 'ERROR: errorThrown=' + errorThrown );
         }
         this; // the options for this ajax request
-    }
+    };
+
+    function sampleFunction(x1,x2,points,func,legend_label) {
+        debug('sampling data for ' + x1 + ' to ' + x2 );
+        var d = [];
+        var step = (x2 - x1)/points;
+        for (var i = x1; i < x2; i += step ) {
+            d.push([i, func( i )]);
+        }
+
+        return [
+            { label: legend_label , data: d }
+        ];
+    };
